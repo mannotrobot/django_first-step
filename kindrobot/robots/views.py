a32dacca6c46e8fec965fcfc9ae077c7f37e535c
mannnotrobot <boxforgit@gmail.com>
2023-09-03 21:18:27 +0300

    using converter for dynamic urls

--- kindrobot/robots/views.py
+++ kindrobot/robots/views.py
@@ -11,5 +11,17 @@ def index(request): # HttpRequest
 
 
 
-def categories(request):
-    return HttpResponse("<h1>Notes by category</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Notes by category</h1><p>id: {cat_id}</p>")
+
+
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Notes by category</h1><p>slug: {cat_slug}</p>")
+
+
+
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Archive by years</h1><p>{year}</p>")
